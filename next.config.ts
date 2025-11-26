8412b55cc21b5d7428a7b1d0d3b8807e15c446f5
ProgramMoh <mohamed.elnaggar@ucalgary.ca>
2025-11-26 13:24:29 -0700

    for deploymenr

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",  // <--- CRITICAL for GitHub Pages
+  images: {
+    unoptimized: true, // <--- CRITICAL: GitHub Pages cannot optimize images on the fly
+  },
 };
 
 export default nextConfig;
